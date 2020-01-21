6aedf14cd1095205745002cbfc4b16d6cf81f0e0
DeniseFafette <denisefafette@gmail.com>
2020-01-21 11:28:59 -0800

    completed post-router.js

--- posts/post-router.js
+++ posts/post-router.js
@@ -7,7 +7,10 @@ const router = express.Router();
 
 router.get('/', async (req, res, next) => {
     try {
-
+        // translates to SELECT * FROM posts
+        //res.json(await db.select("*").from("posts"))
+        // standard to write code below
+        res.json(await db("posts").select())
     } catch (err) {
         next(err)
     }
@@ -15,7 +18,14 @@ router.get('/', async (req, res, next) => {
 
 router.get('/:id', async (req, res, next) => {
     try {
-
+        // short way of writing the same code gets verbose
+        // const post = await db.select("*").from("posts").where("id", req.params.id)
+        // const post = await db("posts").where("id", req.params.id).select()
+        //const post = await db("posts").where("id", req.params.id).limit(1).select()
+        // to limit to one result that does not return an array
+        // below is the standard format of writing this code
+        const post = await db("posts").where("id", req.params.id).first()
+        res.json(post)
     } catch (err) {
         next(err)
     }
@@ -23,7 +33,13 @@ router.get('/:id', async (req, res, next) => {
 
 router.post('/', async (req, res, next) => {
     try {
-
+        const payload = {
+            title: req.body.title,
+            contents: req.body.contents,
+        }
+        // translates to 'INSERT INTO posts (title, contents) VALUES(?, ?);'
+        const [id] = await db("posts").insert(payload)
+        res.json(await db("posts").where("id", id).first())
     } catch (err) {
         next(err)
     }
@@ -31,7 +47,13 @@ router.post('/', async (req, res, next) => {
 
 router.put('/:id', async (req, res, next) => {
     try {
-
+        const payload = {
+            title: req.body.title,
+            contents: req.body.contents,
+        }
+        // translates to 'UPDATE posts SET title = ? AND contents = ? WHERE id = ?;'
+        await db("posts").where("id", req.params.id).update(payload)
+        res.json(await db("posts").where("id", req.params.id).first())
     } catch (err) {
         next(err)
     }
@@ -39,7 +61,9 @@ router.put('/:id', async (req, res, next) => {
 
 router.delete('/:id', async (req, res, next) => {
     try {
-
+        // translates to 'DELETE FROM posts WHERE id = ?;'
+        await db("posts").where("id", req.params.id).del()
+        res.status(204).end()
     } catch (err) {
         next(err)
     }
